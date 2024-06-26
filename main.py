089203f2e44be2b0f3648862eb9521c7a85a23a2
Yeda CUI <cuiyeda@outlook.com>
2024-06-26 10:30:12 +0800

    update

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     parser = get_args()
     args = parser.parse_args()
 
-    args.mode = 'avg_bs_bermudan_put_free_test_with_num_ex_10'
+    args.mode = 'avg_bs_bermudan_put_free_test_num_ex_2_max_1.6'
     args.gpus = 1
     main(vars(args))
