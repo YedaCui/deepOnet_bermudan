742c22998948436f29672dc61939b5fe9b3f3224
Yeda CUI <cuiyeda@outlook.com>
2024-07-21 09:38:29 +0800

    update

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ if __name__ == '__main__':
 
     # args.mode = "avg_bs_bermudan_put_free_test_num_ex_4_qmax_0.1_sensor_type_MP_num_sensor_200"
     # main(vars(args))
-    args.mode = "avg_bs_bermudan_put_free_test_num_ex_12_qmax_0.1_sensor_type_MP_num_sensor_50_var_rescale_2"
+    args.mode = "avg_bs_bermudan_put_free_test_num_ex_12_qmax_0.1_sensor_type_MP_num_sensor_200_var_rescale_2"
     main(vars(args))
